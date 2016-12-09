cfde6f071467c6e1791fb95e64c2885a19231736
Stephen H. Gerstacker <stephen@gerstacker.us>
2016-12-09 10:32:19 -0500

    Added graphing for Day 02, Part 2

--- 09/decode2.rb
+++ 09/decode2.rb
@@ -1,81 +1,106 @@
 #!/usr/bin/env ruby
 
-class Node
-  def self.parse_children(data)
-    children = []
+class Parser
+  def self.parse(data)
+    parser = Parser.new
+    parser.parse(data)
+  end
 
-    state = :none
-    buffer = ''
+  def parse(data)
+    # Initialize state
+    @nodes = []
+    @state = :none
+    @buffer = ''
+    @letters = data.split('')
 
-    letters = data.split('')
-    while letters.any?
-      letter = letters.shift
+    while @letters.any?
+      letter = @letters.shift
 
-      if state == :none
-        buffer += letter
+      if @state == :none
+        @buffer += letter
+        @state = (letter == '(') ? :marker : :word
+      elsif @state == :word
         if letter == '('
-          state = :marker
+          handle_word
+          @buffer = letter
+          @state = :marker
         else
-          state = :word
+          @buffer += letter
         end
-      elsif state == :word
-        if letter == '('
-          children << DataNode.new(buffer) unless buffer.empty?
-          buffer = letter
-          state = :marker
-        else
-          buffer += letter
-        end
-      elsif state == :marker
-        buffer += letter
+      elsif @state == :marker
+        @buffer += letter
         if letter == ')'
-          if buffer =~ /\((\d+)x(\d+)\)/
-            marker = buffer
-            length = $1.to_i
-            count = $2.to_i
-
-            buffer = ''
-            length.times { buffer += letters.shift }
-
-            children << MarkerNode.new(marker, length, count, buffer)
-            buffer = ''
-          else
-            raise "Invalid marker: #{buffer}"
-          end
-
-          state = :none
+          handle_marker
+          @state = :none
         end
       end
     end
 
-    if state == :word && !buffer.empty?
-      children << DataNode.new(buffer)
-    elsif state == :marker
-      if buffer =~ /\((\d+)x(\d+)\)/
-        marker = buffer
-        length = $1.to_i
-        count = $2.to_i
-
-        buffer = ''
-        length.times { buffer += letters.shift }
-
-        children << MarkerNode.new(marker, length, count, buffer)
-      else
-        raise "Invalid marker: #{buffer}"
-      end
+    if @state == :word
+      handle_word
+    elsif @state == :marker
+      handle_marker
     end
 
-    children 
+    @nodes
+  end
+
+  private
+
+  def handle_word
+    @nodes << DataNode.new(@buffer) unless @buffer.empty?
+    @buffer = ''
+  end
+
+  def handle_marker
+    if @buffer =~ /\((\d+)x(\d+)\)/
+      marker = @buffer
+      length = $1.to_i
+      count = $2.to_i
+
+      @buffer = ''
+      length.times { @buffer += @letters.shift }
+
+      @nodes << MarkerNode.new(marker, length, count, @buffer)
+      @buffer = ''
+    else
+      raise "Invalid marker: #{@buffer}"
+    end
+  end
+end
+
+class Node
+  def dots
+    raise NotImplementedError
   end
 
   def length
-    0
+    raise NotImplementedError
+  end
+end
+
+class RootNode < Node
+  def initialize(data)
+    @children = Parser.parse data
+  end
+
+  def dots
+    dots = @children.map { |child| "ROOT -> #{child.object_id}" }
+    dots += @children.map { |child| child.dots }.flatten
+  end
+
+  def length
+    @children.reduce(0) { |sum, x| sum + x.length }
   end
 end
 
 class DataNode < Node
   def initialize(data)
     @data = data
+  end
+
+  def dots
+    ["#{self.object_id}[label=\"#{@data}\"]"]
   end
 
   def length
@@ -88,7 +113,13 @@ class MarkerNode < Node
     @marker = marker
     @marker_length = marker_length
     @marker_count = marker_count
-    @children = Node.parse_children(data)
+    @children = Parser.parse data
+  end
+
+  def dots
+    dots = ["#{self.object_id}[label=\"#{@marker}\"]"]
+    dots += @children.map { |child| "#{self.object_id} -> #{child.object_id}" }
+    dots += @children.map { |child| child.dots }.flatten
   end
 
   def length
@@ -98,9 +129,20 @@ class MarkerNode < Node
 end
 
 data = ARGF.read.chomp
-nodes = Node.parse_children data
+root = RootNode.new data
 
-puts "Nodes: #{nodes.inspect}"
+puts "Nodes: #{root.inspect}"
+puts "Length: #{root.length}"
 
-length = nodes.reduce(0) { |sum,x| sum + x.length }
-puts "Length: #{length}"
+dots = root.dots
+File.open 'graph.gv', 'w' do |f|
+  f.puts 'digraph {'
+
+  dots.each do |dot|
+    f.puts "\t#{dot}"
+  end
+
+  f.puts '}'
+end
+
+`dot graph.gv -Tpng -o graph.png`
